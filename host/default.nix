1e671d25a0d09c968fa5dfe72a9f4911ce0a4e20
ketsumashu <k2mashu@icloud.com>
2024-04-15 18:24:36 +0900

    git wasureteta

---
+++ host/default.nix
@@ -0,0 +1,9 @@
+    { pkgs, inputs, ... }: {
+	
+	fonts.packages = with pkgs; [
+	    noto-fonts
+	    noto-fonts-cjk
+	    (nerdfonts.override { fonts = [ "FiraCode" ]; } )
+	    cozette
+	];
+    }
