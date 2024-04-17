3eb40398d99d48fe78086522083796798f59a1eb
ketsumashu <k2mashu@icloud.com>
2024-04-17 16:33:29 +0900

    uakari..?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.KernelModules = [ "usbhid" "joydev" "xpad" ];
+  boot.initrd.kernelModules = [ "usbhid" "joydev" "xpad" ];
   boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
 
   networking.hostName = "mashunix"; # Define your hostname.
