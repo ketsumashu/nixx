bf8d11c8f6d52aa65563cd4158fba871f857500e
ketsumashu <k2mashu@icloud.com>
2024-04-17 20:01:10 +0900

    uakari..?

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -9,6 +9,128 @@
     enable = true;
     package = waybar.packages."${pkgs.system}".waybar;
     systemd = {enable = false;};
+    style = ''
+
+      * {
+          font-family: "CozetteHiDpi","mplus12";
+          font-size: 13px;
+          min-height: 0;
+      }
+
+      window#waybar {
+          background-color: rgba(25,28,37,0.88);
+          border-top-right-radius:12px;
+          border-bottom-right-radius:12px;
+      }
+
+      window#waybar.hidden {
+          opacity: 0;
+      }
+
+      #workspaces button {
+          padding:  10px 0px;
+          background-color: transparent;
+          color: #e0def4;
+      }
+
+      #workspaces button.persistent {
+          padding:  10px 0px;
+          background-color: transparent;
+          color: #e0def4;
+      }
+
+      #workspaces button:hover {
+          background: rgba(0, 0, 0, 0.2);
+      }
+
+      #workspaces button.active {
+          color: #77adb1;
+      }
+
+      #workspaces button.urgent {
+          background-color: #524f67;
+          color: #e0def4;
+      }
+
+      #mode {
+          background-color: #cf8164;
+          color: #23252e;
+          /*font-weight:bold;*/
+          margin: 0 4px;
+      }
+
+      #memory,
+      #pulseaudio,
+      #cpu,
+      #temperature{
+          padding: 0 0px;
+          color:#a994b8;
+          /*font-weight:bold;*/
+      }
+
+      #window{
+          color:#c5c4d4;
+          padding: 100px 20px;
+      }
+      #workspaces {
+          margin: 0 0px;
+      }
+
+      .modules-left > widget:first-child > #workspaces {
+          margin-left: 0;
+      }
+
+      .modules-right > widget:last-child > #workspaces {
+          margin-right: 0;
+      }
+
+      #network,
+      #clock {
+          color: #889bb4;
+          font-size: 13px;
+          margin-bottom: 2px;
+      }
+
+      @keyframes blink {
+          to {
+              background-color: #f5f5f5;
+              color: #000000;
+          }
+      }
+
+      label:focus {
+          background-color: #000000;
+      }
+
+      #network.disconnected {
+          background-color: #f5f5f5;
+          color: #121214
+      }
+
+      #pulseaudio {
+          color: #77adb1;
+      }
+
+      #pulseaudio.muted {
+          color: #c5c4d4;
+          font-weight: normal;
+      }
+
+      #tray {
+          color: #f5f5f5;
+          padding-bottom:10px;
+      }
+
+      #idle_inhibitor {
+          background-color: #191c25;
+      }
+
+      #idle_inhibitor.activated {
+          background-color: #ecf0f1;
+          color: #2d3436;
+      }
+
+    '';
     settings = {
       mainBar = {
         layer = "top";
@@ -92,128 +214,6 @@
           on-click = "pavucontrol";
         };
       };
-      style = ''
-
-        * {
-            font-family: "CozetteHiDpi","mplus12";
-            font-size: 13px;
-            min-height: 0;
-        }
-
-        window#waybar {
-            background-color: rgba(25,28,37,0.88);
-            border-top-right-radius:12px;
-            border-bottom-right-radius:12px;
-        }
-
-        window#waybar.hidden {
-            opacity: 0;
-        }
-
-        #workspaces button {
-            padding:  10px 0px;
-            background-color: transparent;
-            color: #e0def4;
-        }
-
-        #workspaces button.persistent {
-            padding:  10px 0px;
-            background-color: transparent;
-            color: #e0def4;
-        }
-
-        #workspaces button:hover {
-            background: rgba(0, 0, 0, 0.2);
-        }
-
-        #workspaces button.active {
-            color: #77adb1;
-        }
-
-        #workspaces button.urgent {
-            background-color: #524f67;
-            color: #e0def4;
-        }
-
-        #mode {
-            background-color: #cf8164;
-            color: #23252e;
-            /*font-weight:bold;*/
-            margin: 0 4px;
-        }
-
-        #memory,
-        #pulseaudio,
-        #cpu,
-        #temperature{
-            padding: 0 0px;
-            color:#a994b8;
-            /*font-weight:bold;*/
-        }
-
-        #window{
-            color:#c5c4d4;
-            padding: 100px 20px;
-        }
-        #workspaces {
-            margin: 0 0px;
-        }
-
-        .modules-left > widget:first-child > #workspaces {
-            margin-left: 0;
-        }
-
-        .modules-right > widget:last-child > #workspaces {
-            margin-right: 0;
-        }
-
-        #network,
-        #clock {
-            color: #889bb4;
-            font-size: 13px;
-            margin-bottom: 2px;
-        }
-
-        @keyframes blink {
-            to {
-                background-color: #f5f5f5;
-                color: #000000;
-            }
-        }
-
-        label:focus {
-            background-color: #000000;
-        }
-
-        #network.disconnected {
-            background-color: #f5f5f5;
-            color: #121214
-        }
-
-        #pulseaudio {
-            color: #77adb1;
-        }
-
-        #pulseaudio.muted {
-            color: #c5c4d4;
-            font-weight: normal;
-        }
-
-        #tray {
-            color: #f5f5f5;
-            padding-bottom:10px;
-        }
-
-        #idle_inhibitor {
-            background-color: #191c25;
-        }
-
-        #idle_inhibitor.activated {
-            background-color: #ecf0f1;
-            color: #2d3436;
-        }
-
-      '';
     };
   };
 }
