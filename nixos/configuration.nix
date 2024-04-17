41e1b18e07413b799d008dd4856946ec79fb172f
ketsumashu <k2mashu@icloud.com>
2024-04-17 18:39:10 +0900

    uakari..?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,19 +88,24 @@
     gamescopeSession.enable =true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
-      libkrb5
-      keyutils
-      xorg.libXcursor
-      xorg.libXi
-      xorg.libXinerama
-      xorg.libXScrnSaver
-      libpng
-      libpulseaudio
-      libvorbis
-      stdenv.cc.cc.lib
     ];
   };
-
+  nixpkgs.config.packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
+    };
   # Polkit
   security.polkit.enable = true;
 
