d99311e521e137e76a76f4b3acafdf6d2c21b1ad
ketsumashu <k2mashu@icloud.com>
2024-04-17 20:32:20 +0900

    uakari..?

---
+++ home/starship/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  programs.starship = {
+    enable = true;
+    settings = {
+      character = {
+        success_symbol = "[>](fg:#cdb6dd)";
+        error_symbol = "[x](fg:#d59076)";
+        vimcmd_symbol = "[<](fg:#d59076)";
+      };
+      git_status = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+      git_commit = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+      time = {
+        disabled = false;
+        style = "blue";
+        format = "[$time]($style) ";
+      };
+    };
+  };
+}
+
