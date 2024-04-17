2c454b18d2f691abab01006868b2eef480da7f43
ketsumashu <k2mashu@icloud.com>
2024-04-17 20:37:20 +0900

    uakari..?

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -7,10 +7,9 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
+      starship init fish | source
     '';
   };
-  
-  xdg.configFile."fish/conf.d/nix-env.fish" = ./nix-env.fish;
 
 }
 
