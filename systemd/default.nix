12b30695dfbc4b85e271944298ba3a14a706ee99
ketsumashu <k2mashu@icloud.com>
2024-04-17 21:10:57 +0900

    uakari..?

--- systemd/default.nix
+++ systemd/default.nix
@@ -1,16 +1,13 @@
-# pkgs is used to fetch screen & irssi.
- {pkgs, ...}: 
- {
-   # ircSession is the name of the new service we'll be creating
-   systemd.services.chownX11 = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      description = "chown /tmp/.X11-unix to mashu for executing gamescope in Steam app";
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''${pkgs.coreutils}/bin/chown mashu /tmp/.X11-unix''; 
-        Restart = "no";
-      };
-   };
-   environment.systemPackages = [pkgs.coreutils];
- }
+{pkgs, ...}: {
+  systemd.services.chownX11 = {
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+    description = "chown /tmp/.X11-unix to mashu for executing gamescope in Steam app";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.coreutils}/bin/chown mashu /tmp/.X11-unix'';
+      Restart = "no";
+    };
+  };
+  environment.systemPackages = [pkgs.coreutils];
+}
