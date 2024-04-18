4d6d60cca32bdd384d77f6d142a81a56b777d68f
ketsumashu <k2mashu@icloud.com>
2024-04-19 03:20:25 +0900

    final destination

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,8 +45,8 @@
       enabled = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-gtk
-        fcitx5-qt
         fcitx5-skk
+        libsForQt5.fcitx5-qt
         libsForQt5.fcitx5-configtool
       ];
     };
