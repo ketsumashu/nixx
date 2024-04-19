6e415bd22597558accf1d2dabcfd8d151894cab2
ketsumashu <k2mashu@icloud.com>
2024-04-19 14:09:01 +0900

    complete

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -226,5 +226,9 @@
     name = "Bibata-Modern-Classic";
     size = 11;
   };
-
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 }
