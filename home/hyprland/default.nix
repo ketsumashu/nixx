13192d4c8d85d367f44d78bc16a4a22fa4c55f0a
ketsumashu <k2mashu@icloud.com>
2024-04-20 19:07:56 +0900

    complete

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -25,7 +25,7 @@
     XDG_SESSION_TYPE = "wayland";
     XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_DESKTOP = "Hyprland";
-    QT_QPA_PLATFORM= "wayland;xcb";
+    QT_QPA_PLATFORM= "wayland";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1"; 
   };
 
