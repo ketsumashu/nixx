07c28042bf08e759a0b59dda1cf73fda154ccd1f
ketsumashu <k2mashu@icloud.com>
2024-04-20 22:43:46 +0900

    ''

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -12,12 +12,18 @@
     ./cmp.nix
     ./telescope.nix
     ./statusline.nix
+    ./surround.nix
   ];
 
   programs.nixvim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
-    colorschemes.catppuccin.enable = true;
+    colorschemes.poimandres.enable = true;
+    colorschemes.poimandres.settings = {
+      disable_background = true;
+      disable_float_background = true;
+      disable_italics = true;
+    };
   };
 }
