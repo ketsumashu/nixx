a8c23baa0cbcf981883a6026daa8b5b403bb00da
ketsumashu <k2mashu@icloud.com>
2024-04-20 21:16:06 +0900

    --allow-empty-message

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -8,6 +8,10 @@
     interactiveShellInit = ''
       set fish_greeting
       starship init fish | source
+      abbr -a sw "cd ~/nixx && sudo nixos-rebuild switch"
+      abbr -a ga "git add ."
+      abbr -a gco "git commit --allow-empty-message -m \'\'" 
+      abbr -a gpu "git push"
     '';
   };
 
