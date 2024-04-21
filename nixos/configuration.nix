8f0aea744df584d1997c2ecd9d5e3b5f22892cb8
ketsumashu <k2mashu@icloud.com>
2024-04-21 18:08:59 +0900

    ''

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -154,6 +154,11 @@
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "-d";
+    };
   };
 
   environment.systemPackages = with pkgs; [
