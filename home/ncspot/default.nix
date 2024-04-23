a3aff548b29fd0aea6bdff34074ca28a1e9e3bb9
ketsumashu <k2mashu@icloud.com>
2024-04-23 11:02:32 +0900

    ''

--- home/ncspot/default.nix
+++ home/ncspot/default.nix
@@ -1,12 +1,27 @@
 {pkgs, ... }:{
   programs.ncspot = {
     enable = true;
-    package = (pkgs.ncspot.override{
-        cover = true;
-      });
     settings = {
       use_nerdfont = false;
       gapless = true;
+      theme = {
+        background = "#1B1C28";
+        primary = "#A6ACCD";
+        secondary = "#5DE4C7";
+        title = "#5DE4C7";
+        playing = "#5DE4C7";
+        playing_selected = "#5DE4C7";
+        playing_bg = "#1B1C28";
+        highlight = "#ADD7FF";
+        highlight_bg = "#191C25";
+        error = "#FAE4FC";
+        error_bg = "#1B1C28";
+        statusbar = "#A6ACCD";
+        statusbar_progress = "#5DE4C7";
+        statusbar_bg = "#1B1C28";
+        cmdline = "#CBFFDE";
+        cmdline_bg = "#1B1C28";
+      };
     };
   };
 }
