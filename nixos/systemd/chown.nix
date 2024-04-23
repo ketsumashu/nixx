b8aa23276c5c2d2e0942ad988ef99ac45d36b6ba
ketsumashu <k2mashu@icloud.com>
2024-04-23 17:09:13 +0900

    ''

--- nixos/systemd/chown.nix
+++ nixos/systemd/chown.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   systemd.services.chownX11 = {
     wantedBy = ["multi-user.target"];
-    after = ["multi-user.target"];
+    after = ["network.target"];
     description = "chown /tmp/.X11-unix to mashu for executing gamescope in Steam app";
     serviceConfig = {
       Type = "simple";
