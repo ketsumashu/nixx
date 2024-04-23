7748f71b674b948ffe70f7fbe6475ec98d5f8939
ketsumashu <k2mashu@icloud.com>
2024-04-23 10:44:46 +0900

    ''

--- home/ncspot/default.nix
+++ home/ncspot/default.nix
@@ -1,10 +1,12 @@
 {pkgs, ... }:{
   programs.ncspot = {
     enable = true;
+    package = (pkgs.ncspot.override{
+        cover = true;
+      });
     settings = {
       use_nerdfont = false;
       gapless = true;
-      cover = true;
     };
   };
 }
