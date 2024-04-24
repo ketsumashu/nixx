e722c2e4fdc51abd3805b3bc7b4d2031c523b0b7
ketsumashu <k2mashu@icloud.com>
2024-04-24 23:36:20 +0900

    ''

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -10,7 +10,7 @@
       starship init fish | source
       abbr -a sw "cd ~/nixx && sudo nixos-rebuild switch"
       abbr -a ga "git add ."
-      abbr -a go "git commit --allow-empty-message -m \' \' "
+      abbr -a go "git commit --allow-empty-message -m \" \" "
       abbr -a gu "git push"
       abbr -a nc "sudo nix-collect-garbage -d"
       abbr -a ll "eza -al"
