ba08a7a7df05c76019d862d2a31e914bd978fe0b
ketsumashu <k2mashu@icloud.com>
2026-08-17 17:50:08 +0900

    token:nanka nazo no error

--- home/nvimx/nvim/init.lua
+++ home/nvimx/nvim/init.lua
@@ -3,8 +3,8 @@ vim.g.maplocalleader = " "
 
 vim.loader.enable()
 
-require("config.lazy")
 require("config.options")
+require("config.lazy")
 require("config.extra")
 require("config.zellij")
 require("config.keymaps")
