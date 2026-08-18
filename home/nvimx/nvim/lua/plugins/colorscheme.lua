c6eb81f38892370063bbcfffc1655c7042b09893
ketsumashu <k2mashu@icloud.com>
2026-08-18 23:34:40 +0900

    neovim:substrata does not use setup()

--- home/nvimx/nvim/lua/plugins/colorscheme.lua
+++ home/nvimx/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       vim.g.substrata_italic_comments = false
       vim.g.transparent = true
-      require("substrata").setup()
       vim.cmd.colorscheme("substrata")
     end,
   },
