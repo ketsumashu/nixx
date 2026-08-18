c7c23c52a7936dc2218bd3a483384681abd61e24
ketsumashu <k2mashu@icloud.com>
2026-08-18 23:35:47 +0900

    chore

--- home/nvimx/nvim/lua/plugins/colorscheme.lua
+++ home/nvimx/nvim/lua/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
     priority = 1000,
     config = function()
       vim.g.substrata_italic_comments = false
-      vim.g.transparent = true
       vim.cmd.colorscheme("substrata")
+      vim.g.transparent = true
     end,
   },
 }
