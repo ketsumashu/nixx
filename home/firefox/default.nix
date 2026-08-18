f7cd1415c758d91be67eb869d7739cb02fcb9d7d
ketsumashu <k2mashu@icloud.com>
2026-08-18 19:29:40 +0900

    firefox:back to firefox from zen-beta

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -4,15 +4,17 @@
   ...
 }:
 {
-  imports = [
-    inputs.zen.homeModules.beta
-  ];
+  #imports = [
+  #  inputs.zen.homeModules.beta
+  #];
 
-  programs.zen-browser = {
+  programs.firefox = {
     enable = true;
+    package = pkgs.firefox-beta-bin;
     nativeMessagingHosts = with pkgs; [ tridactyl-native ];
   };
 
   xdg.configFile."tridactyl/themes/poimandres.css".source = ./tridactyl/poimandres.css;
   xdg.configFile."tridactyl/tridactylrc".source = ./tridactyl/tridactylrc;
+
 }
