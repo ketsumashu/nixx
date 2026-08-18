e0680b0113427b847877ad550e2f8ac8b09c63d8
ketsumashu <k2mashu@icloud.com>
2026-08-19 00:08:40 +0900

    linux:update to 7.2

--- nixos/system.nix
+++ nixos/system.nix
@@ -40,7 +40,7 @@
     ];
     supportedFilesystems = [ "ntfs" ];
     blacklistedKernelModules = [ "k10temp" ];
-    kernelPackages = pkgs.linuxPackages_testing;
+    kernelPackages = pkgs.linuxPackages_latest;
     tmp.cleanOnBoot = true;
     extraModulePackages = with config.boot.kernelPackages; [
       zenpower
