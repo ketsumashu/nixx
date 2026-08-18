d5af9c79e3b35fe8e642426103feaca599b776ca
ketsumashu <k2mashu@icloud.com>
2026-08-18 17:46:06 +0900

    kernel:test AMD FRL

--- nixos/system.nix
+++ nixos/system.nix
@@ -49,6 +49,10 @@
     extraModprobeConfig = ''
       options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
     '';
+    kernelParams = [
+      "amdgpu.ppfeaturemask=0xfffd7fff"
+      "amdgpu.dc_feature_mask=0x400"
+    ];
   };
 
   fileSystems."/" = {
