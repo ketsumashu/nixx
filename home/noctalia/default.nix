0210de8f77b8d776b6482a01ac74f1343a3c1bbf
ketsumashu <k2mashu@icloud.com>
2026-08-24 22:24:09 +0900

    noctalia:flake input now only use for packages

--- home/noctalia/default.nix
+++ home/noctalia/default.nix
@@ -1,13 +1,10 @@
 {
   inputs,
   config,
-  lib,
+  pkgs,
   ...
 }:
 {
-  imports = [
-    inputs.noctalia.homeModules.default
-  ];
   xdg.configFile = {
     "noctalia/settings.json".source =
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixx/home/noctalia/settings.json";
@@ -15,5 +12,6 @@
   programs.noctalia = {
     enable = true;
     systemd.enable = true;
+    package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 }
