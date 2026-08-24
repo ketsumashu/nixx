96813d767f7f3075df54ab9ea1bd548a7955c24f
ketsumashu <k2mashu@icloud.com>
2026-08-24 22:57:08 +0900

    treesitter:add nix indent setting

--- home/nvimx/nvim/lua/plugins/treesitter.lua
+++ home/nvimx/nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,13 @@ return {
   {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "nix",
+      callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
   },
 }
