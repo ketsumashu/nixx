7751129093b966197d0c7c95986de849c292ee8d
ketsumashu <k2mashu@icloud.com>
2026-08-27 01:31:07 +0900

    fish:spoof TERM_PROGRAM when exec yazi over herdr

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -13,6 +13,13 @@
       abbr -a nrun "nix run nixpkgs#"
       abbr -a up "cd ~/nixx && nix flake update"
       abbr -a df "duf --theme ansi --output mountpoint,size,used,avail,filesystem --only local,fuse"
+      function yazi
+        if test "$HERDR_ENV" = 1
+          env TERM_PROGRAM=ghostty yazi $argv
+        else
+          command yazi $argv
+        end
+      end
       fish_vi_key_bindings
       bind -M insert -m default jj backward-char force-repaint
     '';
