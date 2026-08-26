237ed2a6865ab1d93bd324c16a7080d18c11c883
ketsumashu <k2mashu@icloud.com>
2026-08-27 02:11:40 +0900

    hyprland:goodbye pseudo-sidecar

--- home/hyprland/hyprland.lua
+++ home/hyprland/hyprland.lua
@@ -87,9 +87,9 @@ hl.bind(mainmod .. "+ mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 --workspace groups
 local workspace_groups = {
-  { range = { 1, }, monitor = "HDMI-A-1" },
-  { range = { 5, }, monitor = "DP-1" },
-  { range = { 7, }, monitor = "DP-2" },
+  { range = { 7, }, monitor = "HDMI-A-1" },
+  { range = { 8, }, monitor = "DP-1" },
+  { range = { 6, }, monitor = "DP-2" },
 }
 for _, group in ipairs(workspace_groups) do
   for _, ws in ipairs(group.range) do
@@ -167,10 +167,9 @@ hl.config({
 
 --monitor settings
 local monitors = {
-  { output = "DP-2",     mode = "2560x1440@100", position = "0x0",       scale = 1 },
-  { output = "HDMI-A-1", mode = "3840x2160@120", position = "2560x0",    scale = 1 },
-  { output = "DP-1",     mode = "2560x1440@120", position = "6400x0",    scale = 1 },
-  { output = "ipad",     mode = "2266x1488@60",  position = "4000x2160", scale = 2 },
+  { output = "DP-2",     mode = "2560x1440@100", position = "0x0",    scale = 1 },
+  { output = "HDMI-A-1", mode = "3840x2160@120", position = "2560x0", scale = 1 },
+  { output = "DP-1",     mode = "2560x1440@120", position = "6400x0", scale = 1 },
 }
 for _, config in ipairs(monitors) do
   hl.monitor(config)
--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,11 +1,5 @@
 { inputs, ... }:
 {
   imports = [ inputs.hyprland.nixosModules.default ];
-
   programs.hyprland.enable = true;
-
-  services.sunshine = {
-    enable = true;
-    openFirewall = true;
-  };
 }
