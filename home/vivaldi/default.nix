afb5abdb475ac44575980f0fd261c1fbfdb0cbc3
ketsumashu <k2mashu@icloud.com>
2026-08-31 22:48:24 +0900

    vivaldi:custom hover tab bar

--- home/vivaldi/default.nix
+++ home/vivaldi/default.nix
@@ -1,8 +1,15 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  home.file.".config/vivaldi-custom/tabbar.css".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixx/home/vivaldi/tabbar.css";
+
   programs.chromium = {
     enable = true;
     package = pkgs.vivaldi;
-    commandLineArgs = [ "--ozone-platform-hint=auto" ];
+    commandLineArgs = [
+      "--ozone-platform-hint=auto"
+      "--enable-wayland-ime"
+      "wayland-text-input-version=3"
+    ];
   };
 }
