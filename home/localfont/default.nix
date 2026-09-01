7421ebf67dfb23b87043889fb0c71dbecf408be2
ketsumashu <k2mashu@icloud.com>
2026-09-01 22:10:50 +0900

    ghostty:add font

--- home/localfont/default.nix
+++ home/localfont/default.nix
@@ -1,1 +1,4 @@
-{ xdg.dataFile."fonts/mplus12medium.otb".source = ./mplus12medium.otb; }
+{
+  xdg.dataFile."fonts/mplus12medium.otb".source = ./mplus12medium.otb;
+  xdg.dataFile."fonts/UnifontExMono.otb".source = ./UnifontExMono.otb;
+}
